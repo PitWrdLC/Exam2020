6ac2c2d4f4fd10614d4866a0e61985e6cae4e6e9
PitWrdLC <bogdanov2.pa@edu.spbstu.ru>
2021-02-12 17:00:59 +0300

    1.1

--- src/exam/exam.kt
+++ src/exam/exam.kt
@@ -11,6 +11,48 @@ fun example(inputName: String): Int {
 
 // Пожалуйста добавьте необходимые параметры,
 // тип результата, и измените имя функции (Shift+F6)
-fun myExamTask() {
-    TODO()
+
+fun myExamTask(inputName: String, racer: String): String {
+    var finalSumm = 0
+    var time = 0
+    var disclamer1 = " "
+    var disclamer2 = ", "
+    // 1 победитесль в град-при
+    // читать построчно
+    // если ганд-при то счетчик +1
+    // поисск имени ( пройти по строчкам)
+    // если нашел имя то присваивается переменная  в итоге которой будте массив слов [name,mark,number]
+    // взять число из массива и приплючовать к имеющемуся
+    //
+    for (lineA in File(inputName).readLines()) {
+
+        if (lineA.contains(Regex("""^(Гран-при)"""))){ // счетчик для победителей(победитель всегда один)
+        time += 1}
+
+        if (lineA.contains(Regex("""$(racer)+"""))&&time == 0) {   // если обнаружен импостер
+            val perem = lineA.split(disclamer2,disclamer1)
+            finalSumm += perem[2].toInt()
+            time -+ 1
+                }
+
+    }
+
+
+    val final = racer + " " + finalSumm.toString()
+    return final
 }
+/*
+Гран-при Австралии
+Хэмилтон, Мерседес, 8
+Боттас, Мерседес, 6
+Леклер, Феррари, 3
+Феттель, Феррари, 2
+Ферстапен, Ред Булл, 1
+
+Гран-при России
+Албон, Ред Булл, 8
+Хэмилтон, Мерседес, 5
+Феттель, Феррари, 4
+Леклер, Феррари, 2
+Боттас, Мерседес, 1
+*/
